533c2d7820ee710c5162bc101560bf62e70bdca0
shenghai <766058573@qq.com>
2014-12-26 17:39:25 +0800

    load ToolBoxPlugin

--- src/code/iobjectsdemo.cpp
+++ src/code/iobjectsdemo.cpp
@@ -7,23 +7,19 @@
 iObjectsDemo::iObjectsDemo(QWidget *parent) : QWidget(parent)
 {
 	setObjectName("iObjectsDemo");
-
-
-
-	QLabel *lable = new QLabel(this);
-	lable->setText(QStringLiteral("我是中文"));
-	setGeometry(200,200,1247,766);
-
-
-	m_pCloseBtn = new QPushButton(this);
-	m_pCloseBtn->setObjectName("CloseBtn");
-	m_pCloseBtn->setFixedHeight(48);
-	m_pCloseBtn->setFixedWidth(48);
-	m_pCloseBtn->setGeometry(width() - 60, 20, m_pCloseBtn->width(), m_pCloseBtn->height());
-
-
-	connect(m_pCloseBtn, SIGNAL(clicked()), this, SLOT(OnCloseBtnClicked()));
-
+	loadControls();
+	if(!loadToolBoxPlugin())
+	{
+		exit(0);
+	}
+	if (!loadMapTabsPlugin())
+	{
+		exit(0);
+	}
+	if (!loadIntelligencePlugin())
+	{
+		exit(0);
+	}
 }
 
 iObjectsDemo::~iObjectsDemo()
@@ -43,4 +39,54 @@ void iObjectsDemo::paintEvent(QPaintEvent* e)
 void iObjectsDemo::OnCloseBtnClicked()
 {
 	this->close();
+}
+
+bool iObjectsDemo::loadToolBoxPlugin()
+{
+	bool bToolbox = false;
+	QDir path("../bin/debug");
+	foreach( QString filename, path.entryList(QDir::Files) )
+	{
+		qDebug()<<filename;
+		QPluginLoader loader( path.absoluteFilePath( filename ) );
+		QObject *couldBeFilter = loader.instance();
+		if( couldBeFilter )
+		{
+			m_pToolBox = qobject_cast<ToolBoxInterface*>( couldBeFilter );
+			if( m_pToolBox )
+			{
+				//bToolbox = true;
+			}
+		}
+	}
+	return bToolbox;
+}
+
+bool iObjectsDemo::loadControls()
+{
+	QLabel *lable = new QLabel(this);
+	lable->setText(QStringLiteral("我是中文"));
+	setGeometry(200,200,1247,766);
+
+
+	m_pCloseBtn = new QPushButton(this);
+	m_pCloseBtn->setObjectName("CloseBtn");
+	m_pCloseBtn->setFixedHeight(48);
+	m_pCloseBtn->setFixedWidth(48);
+	m_pCloseBtn->setGeometry(width() - 60, 20, m_pCloseBtn->width(), m_pCloseBtn->height());
+
+
+	connect(m_pCloseBtn, SIGNAL(clicked()), this, SLOT(OnCloseBtnClicked()));
+
+	return true;
+}
+
+bool iObjectsDemo::loadMapTabsPlugin()
+{
+	return true;
+}
+
+bool iObjectsDemo::loadIntelligencePlugin()
+{
+	return true;
 }
--- src/code/iobjectsdemo.h
+++ src/code/iobjectsdemo.h
@@ -4,6 +4,8 @@
 #include <QtWidgets/QWidget>
 #include <QApplication>
 
+#include "plugin/ToolBox/toolboxinterface.h"
+
 class iObjectsDemo : public QWidget
 {
 	Q_OBJECT
@@ -15,10 +17,19 @@ public:
 protected:
 	void paintEvent(QPaintEvent*);
 
+signals:
+
 private slots:
 	void OnCloseBtnClicked();
+
+private:
+	bool loadControls();
+	bool loadToolBoxPlugin();
+	bool loadMapTabsPlugin();
+	bool loadIntelligencePlugin();
 private:
 	QPushButton* m_pCloseBtn;
+	ToolBoxInterface* m_pToolBox;
 };
 
 #endif // IOBJECTSDEMO_H
